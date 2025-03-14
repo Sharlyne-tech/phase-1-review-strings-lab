c21a3a44a39cc746921d991e270d400e205ccfc2
sharlyne <sharlynsirma@gmail.com>
2025-03-14 12:58:28 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser  = "sharlyn";
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage += `${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!` ;
